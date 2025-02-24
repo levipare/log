70a875f4a4b0b67b4e90090e981ff2a85bf99f82
Levi Pare <levipare@gmail.com>
2025-02-24 09:46:03 -0500

    initial commit

---
+++ log.c
@@ -0,0 +1,20 @@
+#include "log.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+static const char *levels[] = {"debug", "info", "warn", "error", "fatal"};
+
+static const int colors[] = {34, 32, 33, 31, 31};
+
+void _log(int level, const char *file, int line, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    fprintf(stdout, "\033[1m%s:%d:\033[0m \033[1;%dm%s:\033[0m ", file, line,
+            colors[level], levels[level]);
+    vfprintf(stdout, fmt, args);
+    fputc('\n', stdout);
+
+    va_end(args);
+}
